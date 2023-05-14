a1e729c65c9059ff32d4acd2dfb914ecf84c1b5e
Geoffrey Otieno <otienogeoffrey812@gmai.com>
2023-05-14 23:39:09 +0300

    added tree insert method and fixed rightChild and leftChild datatypes

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -3,13 +3,37 @@ package org.example;
 public class Tree {
     private class Node{
         private int value;
-        private int rightChild;
-        private int leftChild;
-
+        private Node rightChild;
+        private Node leftChild;
         private Node(int value){
             this.value = value;
         }
     }
 
     private Node root;
+    public void insert(int value){
+        var node = new Node(value);
+        if (root == null){
+            root = node;
+            return;
+        }
+        var current = root;
+
+        while (true){
+            if (value < current.value){
+                if (current.leftChild == null){
+                    current.leftChild = node;
+                    return;
+                }
+                current = current.leftChild;
+            } else{
+                if (current.rightChild == null){
+                    current.rightChild = node;
+                    return;
+                }
+                current = current.rightChild;
+            }
+        }
+
+    }
 }
